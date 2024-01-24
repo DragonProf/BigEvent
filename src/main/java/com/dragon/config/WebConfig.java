1c78a89f8cad06794bc1547f3e51e5195a701a1a
DragonPro <2362170141@qq.com>
2024-01-24 16:29:49 +0800

    无效的依赖

--- src/main/java/com/dragon/config/WebConfig.java
+++ src/main/java/com/dragon/config/WebConfig.java
@@ -6,15 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration  //当前这个配置对象也要注入到ioc容器中，有专门的配置类容器注解，直接注入
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Autowired  //对于已经注入ioc容器中的对象，可以通过此注解直接使用对象
+    @Autowired
     private LoginInterceptor loginInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        //登录接口和注册接口不拦截
         registry.addInterceptor(loginInterceptor).excludePathPatterns("/user/login", "/user/register");
     }
 }
--- src/main/java/com/dragon/controller/ArticleController.java
+++ src/main/java/com/dragon/controller/ArticleController.java
@@ -9,16 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/article")
 public class ArticleController {
     @GetMapping ("/list")
-    public Result<String> list(/*@RequestHeader(name="Authorization") String token, HttpServletResponse response*/) {
-/*        //验证token
-        try {
-            Map<String,Object> claims = JwtUtil.parseToken(token);
-
-        } catch (Exception e) {
-            //http响应状态码为401
-            response.setStatus(401);
-            return Result.error("未登录");
-        }*/
+    public Result<String> list() {
         return Result.success("所有文章数据..");
     }
 
